8006529ab5e93f02ab3461659506cbbd529016c4
Samuel Goldsmith <samuelrg2005@gmail.com>
2026-02-04 11:42:25 +1300

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ const imagePairs = [
 ]
 export default function Home() {
   return (
-    <div className="min-w-screen flex min-h-screen bg-center bg-cover" style={{ backgroundImage: "url('/BACKGROUND.webp')" }}>
+    <div className="min-w-screen flex min-h-screen bg-center bg-cover" style={{ backgroundImage: "url('/stream.avif')" }}>
       <main className="min-h-screen w-full  flex">
         <div className="w-1/3">
-          <TextBox text="The Kia Mouriora te Kaiwharawhara dashboard is designed to display ecological data about Te Kaiwharawhara. Navigate through information using the navigation bar and learn about the history, health, and conservation efforts of the region."/>
+          <TextBox text="The Kia Mouriora Te Kaiwharawhara dashboard is designed to display ecological data about Te Kaiwharawhara. Navigate through information using the navigation bar and learn about the history, health, and conservation efforts of the region."/>
           <ImageCycle pairs={imagePairs} header="Birds in the Catchment"></ImageCycle>
         </div>
         <div className="w-2/3">
